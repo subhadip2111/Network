18e5ac3d30379ff4681befba7277f2f0c0eda9c1
subhadip <subhadip@m0.ventures>
2025-01-21 21:34:49 +0530

    feat:user-section-updated

--- user-service/src/Utils/validation.js
+++
@@ -1,9 +0,0 @@
-const joi=require('joi');
-const loginValidation=(data)=>{
-    const schema=joi.object({
-        phoneNumber:joi.string().required(),
-    });
-    return schema.validate(data);
-}
-
-module.exports={loginValidation};
