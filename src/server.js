012f44f8b40925a67d437a3d993e8818076bda35
subhadip <subhadip@m0.ventures>
2025-01-21 23:44:19 +0530

    feat:change in server.js

--- src/server.js
+++ src/server.js
@@ -3,13 +3,10 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const helmet=require('helmet')
 require('dotenv').config();
-const {RateLimiterRedis}=require('rate-limiter-flexible')
-const Redis=require('ioredis')
 const userRoute=require('./Routes/userRoute');
 const cors=require('cors');
 const logger = require('./Utils/logger');
-const {rateLimit}=require('express-rate-limit')
-const {RedisStore}=require('rate-limit-redis')
+
 const app=express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -26,45 +23,6 @@ mongoose.connect(process.env.MONGO_URL,{
     console.log(err);
 })
 
-const redisClient=new Redis(process.env.REDIS_HOST)
-redisClient.on('error', (err) => {
-    logger.error('Redis connection error:', err);
-});
-
-
-const reateLimiter=new RateLimiterRedis({
-    storeClient:redisClient,
-    keyPrefix:'middleware',
-    points:10,
-    duration:1
-})
-
-app.use((req,res,next)=>{
-    reateLimiter.consume(req.ip).then(()=>next()).catch(()=>{
-logger.warn(`ratelimit  extended for this Ip${req.ip}`)    
-        res.status(429).send({message:"Too many requests"})
-    })
-
-})
-
-//IP BASED RATE LIMITING
-const endpointsRateLimit=rateLimit({
-    windowMs:15*60*1000,
-    max:100,
-    standardHeaders:true,
-    legacyHeaders:false,
-    handler:(req,res,)=>{
-        logger.warn(`Sensitive end points rateLimit excided for this Ip ${req.ip}`)
-        res.status(429).send({message:`Too many requests `})
-    },
-    store:new RedisStore(
-        {
-            sendCommand:(...args)=>redisClient.call(...args)
-        }
-    )
-   
-})
-app.use(endpointsRateLimit)
 app.use(userRoute);
 app.get('/',(req,res)=>{
     return res.status(200).send({message:"Hello World"})
